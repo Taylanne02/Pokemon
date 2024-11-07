9bda603e945fdf1d41241c2980c218c735790c56
Taylanne Castelo <121128172+Taylanne02@users.noreply.github.com>
2024-11-06 21:30:00 -0300

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,18 @@
+import CapaHome from "./components/CapaHome";
+import ListaCards from "./components/ListaCards";
+import NavbarGlobal from "./components/NavBar";
+
+export default async function Home() {
+  return (
+    <>
+      <div className="relative h-[100vh] w-full overflow-hidden drop-shadow-2xl">
+        <div className="absolute top-0 left-0 right-0 z-30">
+          <NavbarGlobal />
+        </div>
+        <CapaHome />
+      </div>
+      <ListaCards />
+    </>
+  );
+}
+
